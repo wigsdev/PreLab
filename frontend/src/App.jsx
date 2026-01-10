f25e38cffce7333809f355ea50ecc8784798024c
wigsdev <karlwgs1989@gmail.com>
2026-01-10 15:24:11 -0500

    feat(frontend): agregar vista admin, dependencias UI y rutas

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { Toaster } from 'react-hot-toast'; // [NEW]
 import useTheme from './hooks/useTheme';
 import Navbar from './components/layout/Navbar';
 import HomeView from './views/HomeView';
 import ExamView from './views/ExamView';
 import DashboardView from './views/DashboardView';
+import ProfileView from './views/ProfileView'; // [NEW]
+import CreateQuestionView from './views/admin/CreateQuestionView'; // [NEW]
 import LoginView from './views/LoginView';
 import RegisterView from './views/RegisterView';
 import PrivateRoute from './components/PrivateRoute';
@@ -15,6 +18,7 @@ function App() {
   return (
     <Router>
       <AuthProvider>
+        <Toaster position="top-center" reverseOrder={false} />
         <div className="min-h-screen bg-slate-50 dark:bg-slate-950 transition-colors duration-300 font-sans">
           <Navbar theme={theme} toggleTheme={toggleTheme} />
           <div className="max-w-5xl mx-auto px-4 py-4">
@@ -26,6 +30,16 @@ function App() {
               <Route path="/dashboard" element={
                 <PrivateRoute>
                   <DashboardView />
+                </PrivateRoute>
+              } />
+              <Route path="/profile" element={
+                <PrivateRoute>
+                  <ProfileView />
+                </PrivateRoute>
+              } />
+              <Route path="/admin/create" element={
+                <PrivateRoute>
+                  <CreateQuestionView />
                 </PrivateRoute>
               } />
             </Routes>
